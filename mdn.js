76d22613b95ec8a22ecf749f5ca39b7b3a55f9dd
Muhammad Raza <imuhammadraza53@gmail.com>
2026-08-18 23:32:29 +0500

    Practice for...of loop

---
+++ mdn.js
@@ -0,0 +1,51 @@
+// for loop
+
+// for (let i = 1; i <= 14; i += 2){
+//          console.log(i);     
+// }
+
+
+// for in loop 
+
+// const person = {name:"ali", age : 24, city:"Lahore"};
+
+// for (let key in person){
+//            console.log(key, ":", person[key]);   
+// }
+
+
+// for of loop
+
+// let fruits = ["Apple", "Banana", "Mango"];
+//     for(let fruit of fruits){
+//               console.log(fruit);
+//     }
+
+
+// for each
+
+// const number = [2,4,7];
+
+// number.forEach(function(num){
+//            console.log(num);
+// })
+
+
+// while loop
+
+// let i = 0 ;
+
+// while(i < 5){
+//       console.log("Number : " + i);
+//       i++        
+// }
+
+
+// do while loop
+
+let i = 0 ;
+  do{
+     console.log("run : " , i);
+     i++         
+  }
+  while(i < 3);
