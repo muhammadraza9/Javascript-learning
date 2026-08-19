cbb9b3da495673cab8caf0fd78a8da8e71187af7
Muhammad Raza <imuhammadraza53@gmail.com>
2026-08-19 19:15:16 +0500

    Add JS functions, arrow functions, and scope examples

--- scope.js
+++ scope.js
@@ -1,17 +1,87 @@
-let a = 10;
+// let a = 10;
 
-function outer() {
-     let b = 20;
+// function outer() {
+//      let b = 20;
      
      
-     function inner(){
-        let c = 34;
+//      function inner(){
+//         let c = 34;
         
-        console.log(a);
-        console.log(b);
-        console.log(c);
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
 
-     }
-     inner();
+//      }
+//      inner();
+// }
+// outer();
+
+
+
+
+// Globel Variable
+
+const x = 12;
+
+console.log(x);
+
+//
+
+const a = 10;
+
+function num(){
+   console.log(a);
 }
-outer();
+
+num();
+
+
+// functional scope
+
+function scope(){
+      let x = 20;
+      console.log(x);
+}
+
+scope();
+
+
+
+//
+
+function myfunc(){
+      var name = "raza";
+      console.log(name);
+}
+
+myfunc();
+//console.log(name);
+
+
+
+// Block Scope
+
+{
+   let y = 45;
+   var z = 56;
+
+      console.log(y);
+      console.log(z);
+}
+
+//console.log(y);
+//console.log(z);
+
+
+//
+
+{
+   let x = "in block";
+       console.log(x);
+}
+
+{
+   let x = "in block scope";
+       console.log(x);
+}
+
